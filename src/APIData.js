39134e99b43b6bb49cdb4b1cfc2b94ba37120f1e
shubhamd25 <dubeyshubham902@gmail.com>
2021-10-19 12:38:09 -0700

    first commit

---
+++ src/APIData.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+
+const APIData = () => {
+
+    const [data, setData] = useState([]);
+
+    const getCovidData = async () => {
+        const res = await fetch('https://data.covid19india.org/data.json');
+        const actualData = await res.json();
+        console.log(actualData.statewise);
+        setData(actualData.statewise);
+    }
+
+    useEffect(() => {
+        getCovidData();
+    }, []);
+    return (
+
+        <div>
+
+            <h1>India Covid 19 Dashboard</h1>
+
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>State</th>
+                        <th>Confirmed</th>
+                        <th>Recovered</th>
+                        <th>Deaths</th>
+                        <th>Active</th>
+                        <th>Updates</th>
+                    </tr> 
+                </thead>
+                <tbody>
+                    {
+                        data.map((curElem, ind) => {
+                            return (
+                                <tr>
+                                    <td>{curElem.state}</td>
+                                    <td>{curElem.confirmed}</td>
+                                    <td>{curElem.recovered}</td>
+                                    <td>{curElem.deaths}</td>
+                                    <td>{curElem.actives}</td>
+                                    <td>{curElem.lastupdatedtime}</td>
+                                </tr>
+                            );
+                        })
+                    }
+                </tbody>
+            </table>
+
+        </div>
+    );
+
+}
+
+export default APIData;
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import APIData from './APIData';
+
+function App() {
+
+  return (
+    <>
+      <APIData />
+
+    </> 
+  );
+}
+
+export default App;
